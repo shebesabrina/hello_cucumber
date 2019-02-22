6af83a9a2da205818efcb9648295f17c1f102164
Sabrina Robinson <sabrinarob@gmail.com>
2019-02-22 10:04:19 -0700

    TEST - Implements Friday response

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -9,6 +9,10 @@ Given("today is Sunday") do
   @today = 'Sunday'
 end
 
+Given("today is Friday") do
+  @today = 'Friday'
+end
+
 When("I ask whether it's Friday yet") do
   @actual_answer = is_it_friday(@today)
 end
